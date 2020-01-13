5042c0d6af4d25c751fc006c86071a2b63c72a7e
Mateus Rodrigues <mateussr2511@gmail.com>
2020-01-12 23:37:58 -0300

    whatsapp popup

--- scripts.js
+++ scripts.js
@@ -20,3 +20,25 @@ $('#teamSlide').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+
+
+(function () {
+    var options = {
+        whatsapp: "+55 63 9205-7905", // WhatsApp number
+        call_to_action: "Olá! Como podemos ajudá-lo?", // Call to action
+        position: "right" // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol,
+        host = "getbutton.io",
+        url = proto + "//static." + host;
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = url + "/widget-send-button/js/init.js";
+    s.onload = function () {
+        WhWidgetSendButton.init(host, proto, options);
+    };
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(s, x);
+})();
